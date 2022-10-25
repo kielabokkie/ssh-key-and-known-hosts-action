fd3b0d6032f9b6221aa4e58f975be862e1a27697
Wouter <kielabokkie@gmail.com>
2022-10-25 23:23:00 +1300

    Fix cleanup failure on macOS Github runners (#25)
    Finds the process id of the ssh-agent by using `ps` instead of `pidof`
    
    Closes #24

--- cleanup.js
+++ cleanup.js
@@ -3,7 +3,7 @@ const execa = require('execa')
 async function run() {
   console.log('Stopping ssh-agent')
 
-  await execa('kill -9 $(pidof ssh-agent)', { shell: true });
+  await execa('kill -9 $(ps -e | grep -m1 "[s]sh-agent" | awk \'{print $1}\')', { shell: true });
 }
 
 run()
